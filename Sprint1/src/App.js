d4d6cc88fd960ffc4a24b46e95190257174bbdef
Preyasha Patel <45910402+preyasha2810@users.noreply.github.com>
2022-02-16 15:21:15 -0600

    Frontend using react

---
+++ Sprint1/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import LandingPage from './components/pages/LandingPage'
+import LoginPage from './components/pages/LoginPage'
+import RegisterPage from './components/pages/RegisterPage'
+import ForgetPasswordPage from './components/pages/ForgetPasswordPage'
+import HomePage from './components/pages/HomePage'
+
+import './App.css'
+
+export default function App() {
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route exact path="/" component={ LandingPage } />
+                    <Route path="/login" component={ LoginPage } />
+                    <Route path="/register" component={ RegisterPage } />
+                    <Route path="/forget-password" component={ ForgetPasswordPage } />
+                    <Route path="/home" component={ HomePage } />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    )
+}
+
+const Footer = () => {
+    return (
+        <p className="text-center" style={ FooterStyle }>Designed & coded by <a href="https://github.com/preyasha2810">Preyasha</a></p>
+    )
+}
+
+const FooterStyle = {
+    background: "#000",
+    fontSize: "20px",
+    color: "#fff",
+    position: "absolute",
+    bottom: 0,
+    padding: "1rem",
+    margin: 0,
+    width: "100%",
+}
